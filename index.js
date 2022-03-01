5c0e7b34cc0ffed5db7c85a241433b6aa7913506
Alexia Velador <alexiavel01@gmail.com>
2022-03-01 16:48:43 -0700

    commit tree 43118a0d295998949e669566eec71c07d1147d66

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.innerHTML = ' is the champion';
+
+
+// const h1 = document.getElementById('victory');
+// main.innerHTML = "<h1>i</h1>";
+
+//document.h1.append("victory");
+//const newHeader2 = document.createElement("victory");
+
+// const newHeader = document.getElementById("h1");
+// main.innerHtml = "<h1>victory</h1>";
+//h1.textContent = "victory";
+//const victory = document.getElementById("h1");
+
